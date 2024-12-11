662b15d8cdb93c57cea5cffed5795a299c452183
Kirankk7767 <kiranchennu317@gmail.com>
2024-12-11 17:36:30 +0530

    Update output.tf

--- aws_basic/output.tf
+++ aws_basic/output.tf
@@ -3,32 +3,32 @@ output "instance_id" {
   value       = aws_instance.cda_instance.id
 }
 
-output "public_ip" {
+output "instance_public_ip" {
   description = "The public IP address of the EC2 instance"
   value       = aws_instance.cda_instance.public_ip
 }
 
-output "private_ip" {
+output "instance_private_ip" {
   description = "The private IP address of the EC2 instance"
   value       = aws_instance.cda_instance.private_ip
 }
 
-output "ami_id" {
+output "instance_state" {
+  description = "The state of the EC2 instance"
+  value       = aws_instance.cda_instance.state
+}
+
+output "instance_ami" {
   description = "The AMI ID used for the EC2 instance"
   value       = aws_instance.cda_instance.ami
 }
 
-output "instance_type" {
-  description = "The instance type of the EC2 instance"
-  value       = aws_instance.cda_instance.instance_type
-}
-
-output "security_group_ids" {
-  description = "The security group IDs associated with the EC2 instance"
+output "instance_security_group_id" {
+  description = "The security group ID associated with the EC2 instance"
   value       = aws_instance.cda_instance.vpc_security_group_ids
 }
 
-output "key_name" {
-  description = "The key pair name associated with the EC2 instance"
-  value       = aws_instance.cda_instance.key_name
+output "admin_password" {
+  description = "The administrator password for the Windows EC2 instance"
+  value       = aws_instance.cda_instance.password_data
 }
