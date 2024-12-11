0e9904e52567708effdc08e17385fef34a1fb070
Kirankk7767 <kiranchennu317@gmail.com>
2024-12-11 19:13:46 +0530

    Update output.tf

--- aws_basic/output.tf
+++ aws_basic/output.tf
@@ -1,64 +1,61 @@
-# Output the instance ID of the created EC2 instance
-output "instance_id" {
-  description = "The ID of the created EC2 instance"
-  value       = aws_instance.cda_instance.id
+# Output the admin password directly (since you know it from the user data script)
+output "admin_password" {
+  description = "The Windows Administrator password that was set in user_data"
+  value       = "InterOP@6264"
 }
 
-# Output the public IP address of the created EC2 instance
-output "public_ip" {
-  description = "The public IP address of the created EC2 instance"
-  value       = aws_instance.cda_instance.public_ip
+# Output the username created in the user_data script
+output "admin_username" {
+  description = "The username of the newly created administrator"
+  value       = "admin"
 }
 
-# Output the private IP address of the created EC2 instance
-output "private_ip" {
-  description = "The private IP address of the created EC2 instance"
-  value       = aws_instance.cda_instance.private_ip
-}
-
-# Output the public DNS of the created EC2 instance
-output "public_dns" {
-  description = "The public DNS of the created EC2 instance"
-  value       = aws_instance.cda_instance.public_dns
-}
-
-# Output the instance state (running, stopped, etc.)
-output "instance_state" {
-  description = "The state of the created EC2 instance"
-  value       = aws_instance.cda_instance.instance_state
-}
-
-# Output the tags associated with the instance
-output "instance_tags" {
-  description = "The tags of the created EC2 instance"
-  value       = aws_instance.cda_instance.tags
-}
-
-# Output the user data script for reference
-output "user_data_script" {
-  description = "The user data script for the EC2 instance"
-  value       = aws_instance.cda_instance.user_data
-}
-
-# Output the instance AMI ID
+# Other outputs for instance details remain the same
 output "ami_id" {
   description = "The AMI ID used for the EC2 instance"
   value       = aws_instance.cda_instance.ami
 }
 
-# Output the security group ID associated with the instance
-output "security_group_id" {
-  description = "The security group ID of the EC2 instance"
-  value       = aws_instance.cda_instance.vpc_security_group_ids
+output "instance_id" {
+  description = "The ID of the created EC2 instance"
+  value       = aws_instance.cda_instance.id
 }
 
-# Output the instance type used
+output "instance_state" {
+  description = "The state of the created EC2 instance"
+  value       = aws_instance.cda_instance.instance_state
+}
+
+output "instance_tags" {
+  description = "The tags of the created EC2 instance"
+  value       = aws_instance.cda_instance.tags
+}
+
 output "instance_type" {
   description = "The instance type of the EC2 instance"
   value       = aws_instance.cda_instance.instance_type
 }
 
-# Output the EBS block device volume size
+output "public_ip" {
+  description = "The public IP address of the created EC2 instance"
+  value       = aws_instance.cda_instance.public_ip
+}
+
+output "private_ip" {
+  description = "The private IP address of the created EC2 instance"
+  value       = aws_instance.cda_instance.private_ip
+}
+
+output "public_dns" {
+  description = "The public DNS of the created EC2 instance"
+  value       = aws_instance.cda_instance.public_dns
+}
+
+output "security_group_id" {
+  description = "The security group ID of the EC2 instance"
+  value       = aws_instance.cda_instance.vpc_security_group_ids
+}
+
 output "ebs_volume_size" {
   description = "The size of the EBS block device attached to the instance"
   value       = [for ebs in aws_instance.cda_instance.ebs_block_device : ebs.volume_size][0]
