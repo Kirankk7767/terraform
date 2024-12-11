64036a437c6491610e7016d7ec984df0dca10a76
Kirankk7767 <kiranchennu317@gmail.com>
2024-12-11 13:57:43 +0530

    Update main.tf

--- aws_basic/main.tf
+++ aws_basic/main.tf
@@ -1,20 +1,25 @@
 variable "aws_access_key" {
   default = "AKIATBRPP77VCQ632VGG"
 }
+
 variable "aws_secret_key" {
   default = "34vVeYy8PWCRvmmSdGvjd+dD41jThtwp7F9SSCBT"
 }
+
 variable "aws_ami" {
-  default = "ami-016028889ceebfc33"
+  default = "ami-016028889ceebfc33"  # Make sure this is a Windows AMI
 }
+
 variable "aws_security_group_id" {
   default = "sg-0c450499f49b3f38e"
 }
+
 variable "instance_type" {
   default = "t2.micro"
 }
+
 variable "key_name" {
-  default = "kk1"
+  default = "kk1"  # For SSH key, we can ignore it if we want to use RDP
 }
 
 provider "aws" {
@@ -28,4 +33,21 @@ resource "aws_instance" "cda_instance" {
   instance_type          = var.instance_type
   vpc_security_group_ids = [var.aws_security_group_id]
   key_name               = var.key_name
+
+  user_data = <<-EOF
+              <powershell>
+              # Create a new administrator user
+              net user admin InterOP@6264 /add
+              net localgroup administrators admin /add
+              </powershell>
+              EOF
+
+  # Ensure RDP is enabled and accessible
+  associate_public_ip_address = true
 }
+
+# Optional: Output the Windows Admin password (AWS provides an encrypted password for Administrator user)
+output "admin_password" {
+  value = aws_instance.cda_instance.password_data
+}
+
